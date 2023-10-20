d128b830887490bff36dc417a679dcca3fadc15b
Gerson PAS <sobrinho.gerson@gmail.com>
2023-10-20 10:52:23 -0300

    Adionado arquivos do Curso Java Udemy.

---
+++ CursoUdemi23/src/application/Exercicio2.java
@@ -0,0 +1,21 @@
+package application;
+
+public class Exercicio2 {
+	public static void main(String[] args) {
+		int time = 20;
+		String result = (time > 18) 
+		?
+		 "Good day." : "Good evening.";
+		
+		System.out.println(result); 
+		
+		
+		String[] cars = {"Volvo", "BMW", "Ford"};
+		for(String i : cars) {
+		  System.out.println(i);
+		}
+				
+	}
+
+
+}
---
+++ CursoUdemi23/src/application/Exercicios.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Exercicios {
+
+	 public void myMethod() {
+		    System.out.println("Alô Mundo Java!");
+		  }
+
+		  public static void main(String[] args) {
+			  Exercicios myObj = new Exercicios();    
+			  myObj.myMethod();
+		  }
+		}
+
